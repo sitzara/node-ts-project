3a342dc14f9949368296a93849332aa8e1863361
sitzara <sitzara@gmail.com>
2019-07-31 02:53:58 +0300

    koa

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-const mult = (a: number, b: number): number => a + b;
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
 
 
-mult(1, 2);
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx, next): Promise<void> => {
+  ctx.status = 200;
+  ctx.body = 'Hello, world!';
+  next();
+});
+
+app.use(bodyParser({
+  onerror: (err, ctx): void => {
+    ctx.throw('body parse error', err, 422);
+  }
+}));
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
+
+export default app;
